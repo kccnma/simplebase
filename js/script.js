c7388e3326b548d43d0b19051e5fba3076567004
kccnma <gargiulo@hawaii.edu>
2024-10-04 12:21:52 -1000

    added toggle menu and custom addon examples

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// TOGGLE MENU
+const toggleMenu = document.querySelector('.toggle-menu');
+const toggleMenuButton = document.querySelector('.site-menu-button');
+const toggleMenuLinks = document.querySelectorAll('.toggle-menu a');
+// TOGGLE THE MENU WHEN USER CLICKS ON MENU BUTTON
+toggleMenuButton.onclick = () => {
+    if (toggleMenu.getAttribute('data-menustate') === 'closed') {
+      // if closed, open it    
+      toggleMenu.setAttribute('data-menustate','open');
+    } else {
+      // else, close it
+      toggleMenu.setAttribute('data-menustate','closed');
+    }
+};
+// CLOSE THE MENU WHEN A USER CLICKS ON ANY LINK
+toggleMenuLinks.forEach((el) => {
+  el.onclick = () => {
+    toggleMenu.setAttribute('data-menustate','closed');
+  }
+});
